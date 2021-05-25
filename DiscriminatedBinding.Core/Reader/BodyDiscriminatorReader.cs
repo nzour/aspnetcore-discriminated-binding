8787cbca663d1a8d48de087a429a0c5f682efc4a
zaur <zaur.nadiraliev.228@gmail.com>
2021-05-25 13:10:55 +0300

    Implemented support FromBody source if we use Newtonsoft json serialization

--- DiscriminatedBinding.Core/Reader/BodyDiscriminatorReader.cs
+++ DiscriminatedBinding.Core/Reader/BodyDiscriminatorReader.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using DiscriminatedBinding.Core.Exceptions;
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json.Linq;
 
 namespace DiscriminatedBinding.Core.Reader
 {
@@ -30,9 +31,23 @@ namespace DiscriminatedBinding.Core.Reader
 
         public async Task<string?> ReadDiscriminatorAsync(string property, HttpContext context)
         {
-            context.Request.EnableBuffering();
+            var inMemoryStreamCopy = new MemoryStream();
 
-            using var streamReader = new HttpRequestStreamReader(context.Request.Body, Encoding.Default);
+            // If we use FileBufferingReadStream (see context.Request.EnableBuffering()) https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.webutilities.filebufferingreadstream?view=aspnetcore-5.0
+            // then Newtonsoft's input formatter always disposes it.
+            //
+            // If we will use default tream, then Newtonsoft's input formatter will create FileBufferingReadStream and then dispose.
+            // But we need to read several times from Stream, so we need seakable stream (in case to rewind it)
+            // That's why we need to copy default stream (actually is instance of HttpRequestStream) to any other seakable stream
+            // Warning: maybe not memory safe - if body is too large.
+            //
+            // Maybe we should do this manipulation only when formatter is Newtonsoft's ? 🤔
+            await context.Request.Body.CopyToAsync(inMemoryStreamCopy);
+            await context.Request.Body.DisposeAsync();
+
+            context.Request.Body = inMemoryStreamCopy;
+            context.Request.Body.Position = 0;
+            context.Response.RegisterForDisposeAsync(context.Request.Body);
 
             var formatterContext = new InputFormatterContext(
                 httpContext: context,
@@ -52,14 +67,18 @@ namespace DiscriminatedBinding.Core.Reader
 
             var formattingResult = await formatter.ReadAsync(formatterContext);
 
-            // rewinding the stream
+            // rewinding stream
             context.Request.Body.Position = 0;
 
             return formattingResult.Model switch
             {
                 // System.Text.Json
                 JsonElement jsonText => HandleJsonTextResult(jsonText, property),
-                // todo: implement other formatter like NewtonSoftJson
+
+                // Newtonsoft.Json
+                JObject jObject => HandleNewtonSoftJsonResult(jObject, property),
+
+                // Others not supported yet 
                 _ => null
             };
         }
@@ -77,6 +96,12 @@ namespace DiscriminatedBinding.Core.Reader
             }
 
             return null;
+        }
+
+        private static string? HandleNewtonSoftJsonResult(JObject jObject, string property)
+        {
+            // fixme support different naming strategies
+            return jObject.Value<string>(property);
         }
     }
 }
